beeca12a567e4f1039db94e38881b447295e004f
samdegen <32651420+samdegen@users.noreply.github.com>
2020-09-05 20:41:59 -0600

    Create autofilltest.js

---
+++ autofilltest.js
@@ -0,0 +1,8 @@
+    document.getElementById("first_name").value = "Mary-Anne";
+    document.getElementById("last_name").value = "Thelma";
+    document.getElementById("email").value = mathelma@gmail.com;
+    document.getElementById("phone").value = 3030459285;
+    document.getElementById("address").value = "3549 Columbia Dr.";
+    document.getElementById("city").value = "A Pineapple under the sea";
+    document.getElementById("zipcode").value = 98724;
+    document.getElementById("state").value = "MA";
