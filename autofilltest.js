7c96af4c23436daffdefe2b34dc0a8dcb2755123
samdegen <32651420+samdegen@users.noreply.github.com>
2020-09-06 21:36:26 -0600

    Delete autofilltest.js

--- autofilltest.js
+++
@@ -1,8 +0,0 @@
-    document.getElementById("first_name").value = "Mary-Anne";
-    document.getElementById("last_name").value = "Thickpenis";
-    document.getElementById("email").value = "mathelma@gmail.com";
-    document.getElementById("phone").value = 3030459285;
-    document.getElementById("address").value = "3549 Columbia Dr.";
-    document.getElementById("city").value = "A Pineapple under the sea";
-    document.getElementById("zipcode").value = 98724;
-    document.getElementById("state").value = "MA";
